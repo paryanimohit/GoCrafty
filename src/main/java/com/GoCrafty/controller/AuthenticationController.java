7c3e5b73cc53929221a36a3fc1a61f8fb871b8f9
Mohit Paryani <46473952+paryanimohit@users.noreply.github.com>
2020-11-13 17:09:21 -0500

    Authentication Module

--- src/main/java/com/GoCrafty/controller/AuthenticationController.java
+++ src/main/java/com/GoCrafty/controller/AuthenticationController.java
@@ -9,9 +9,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
-
-import com.GoCrafty.entity.Instructor;
-import com.GoCrafty.entity.Student;
 import com.GoCrafty.service.AuthenticationService;
 
 @Controller
@@ -23,33 +20,10 @@ public class AuthenticationController {
 	@PostMapping("/login")
 	public String login(@ModelAttribute("role")String role,@ModelAttribute("theUser") Object theUser,Model theModel,@SessionAttribute(name="tempSession") HashMap<String,String> studentSession)
 	{
-//		Student student = null;
-//		Instructor instructor =null;
-//		
-//		if(role.equals("student"))
-//		{
-//			student=(Student) theUser;	
-//		}
-//		
-//		else if(role.equals("instructor"))
-//		{
-//			instructor=(Instructor) theUser;
-//		}
-//		
-//		String email = theUser;
-//		String password = student.getPassword();
-//		if (email.isEmpty() || password.isEmpty())
-//		{
-//			m.addAttribute("message","Incorrect Email or Password");
-//			return "redirect:/home/showUserLogin";
-//		}
-//		
-//	 
 		String id = authenticationService.userLogin(theUser,role);
 		
 		if(id == null)
 		{
-			
 			theModel.addAttribute("message","Incorrect Email or Password");
 			return "redirect:/home/showUserLogin";
 		}
@@ -58,6 +32,4 @@ public class AuthenticationController {
 			return "redirect:/home/student/viewProfile";
 			}
 	}
-
-
 }
